04ede6194efa65614b7a521b7992da7e10e1eb6d
garima shrestha <garimashrestha20@gmail.com>
2025-01-23 13:42:06 +0545

    Password was converted to text from varchar in table

--- BlogBackend/config/db.js
+++ BlogBackend/config/db.js
@@ -26,7 +26,7 @@ export const createTable = async () => {
         id SERIAL PRIMARY KEY,
         username VARCHAR(50) NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL
+        password TEXT NOT NULL
         );
     `;
       await pool.query(query);
