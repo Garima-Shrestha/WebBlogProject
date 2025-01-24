ef4352f7812d9ba10e54b7cfc7f1e655d6e018ed
garima shrestha <garimashrestha20@gmail.com>
2025-01-24 17:33:36 +0545

    small change made

--- BlogBackend/models/AuthModel.js
+++ BlogBackend/models/AuthModel.js
@@ -2,7 +2,7 @@ import {pool} from '../config/db.js';
 
 export const createUser = async (username, email, password) => {
     try {
-      const query = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *';
+      const query = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *`;
       const values = [username, email, password];
       const result = await pool.query(query, values);
       return result.rows[0];
