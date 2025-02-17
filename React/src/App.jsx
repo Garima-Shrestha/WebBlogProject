94298dc6294ebc6c607a852a5ef1da7d8067035c
garima shrestha <garimashrestha20@gmail.com>
2025-02-17 14:30:56 +0545

    Lazy loading added in app.jsx

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,40 +1,40 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 
 import './App.css';
-import LoginPage from './components/public/login';
-import RegisterPage from './components/public/Register';
-import HeaderSection from './components/layout/Header';
-import CustomerPage from './components/private/Customer';
-import ContactPage from './components/private/Contacts'
-import MakeABlogPage from './components/private/MakeABlog';
-import HomePage from './components/private/HomePage';
-import BlogPage from './components/private/Blog';
-import DeleteAccountPage from './components/private/DeleteAccount';
-import BloggerProfileViewPage from './components/admin/BloggerProfileView';
-import BlogViewPage from './components/admin/BlogView';
 import PrivateRoute from './components/layout/PrivateRoute'; 
 import PublicRoute from './components/layout/PublicRoute';
 import AdminHeaderSection from './components/layout/AdminHeader';
-import YourBlogPage from './components/private/YourBlog';
+import HeaderSection from './components/layout/Header';
 import FooterSection from './components/layout/Footer';
+
+
+const LoginPage = lazy(() => import('./components/public/login'));
+const RegisterPage = lazy(() => import('./components/public/Register'));
+const CustomerPage = lazy(() => import('./components/private/Customer'));
+const ContactPage = lazy(() => import('./components/private/Contacts'));
+const MakeABlogPage = lazy(() => import('./components/private/MakeABlog'));
+const HomePage = lazy(() => import('./components/private/HomePage'));
+const BlogPage = lazy(() => import('./components/private/Blog'));
+const DeleteAccountPage = lazy(() => import('./components/private/DeleteAccount'));
+const BloggerProfileViewPage = lazy(() => import('./components/admin/BloggerProfileView'));
+const BlogViewPage = lazy(() => import('./components/admin/BlogView'));
+const YourBlogPage = lazy(() => import('./components/private/YourBlog'));
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token')); 
   const user = JSON.parse(localStorage.getItem("user")); 
   const role = user ? user.role : null; 
 
-
   // Store token in localStorage when it changes
   useEffect(() => {                                    
     if (token) {                                          
-      localStorage.setItem('token', token);            //Token exist garxa then save in local storage
+      localStorage.setItem('token', token);            // Token exists, save in local storage
     } else {
-      localStorage.removeItem('token');                //Token exist gardaina remove from local storage
+      localStorage.removeItem('token');                // Token does not exist, remove from local storage
     }
-  }, [token]);                                         //The dependency array [token] tells React to only run the useEffect when the token value changes. 
-                                                        // If the token value stays the same between renders, the effect will not run.
+  }, [token]);                                         // The dependency array [token] tells React to only run the useEffect when the token value changes. 
 
   return (
     <Router>
@@ -43,10 +43,10 @@ function App() {
   );
 }
 
-function LocationWrapper({setToken, token, role}) {
+function LocationWrapper({ setToken, token, role }) {
   const location = useLocation();
   console.log('Current Path:', location.pathname);  
-  console.log('User Role:', role); 
+  console.log('User  Role:', role); 
 
   return (
     <>
@@ -56,22 +56,20 @@ function LocationWrapper({setToken, token, role}) {
       ) : (
         location.pathname !== '/' && location.pathname !== '/login' && 
         location.pathname !== '/register' && <HeaderSection setToken={setToken} />
-        )}                                                                    {/* header is kept here not above because if we keep header below the routes then: 
-                                                                              the routes would be displayed first, and only then would the header appear. */}
+      )}                                                                                    {/* header is kept here not above because if we keep header below the routes then: 
+                                                                                            the routes would be displayed first, and only then would the header appear. */}
 
-      <Routes>
-        {/* Redirection */}
-        <Route path="/"  element={role === "admin" ? <Navigate to="/profileview" /> : role ? <Navigate to="/home" /> : <LoginPage setToken={setToken} />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/* Redirection */}
+          <Route path="/" element={role === "admin" ? <Navigate to="/profileview" /> : role ? <Navigate to="/home" /> : <LoginPage setToken={setToken} />} />
 
-        {/* Public Routes */}
+          {/* Public Routes */}
+          <Route path="/login" element={<PublicRoute><LoginPage setToken={setToken} /></PublicRoute>} />
+          <Route path="/register" element={<PublicRoute><RegisterPage /></PublicRoute>} />
 
-        <Route path="/login" element={ <PublicRoute> <LoginPage setToken={setToken} /> </PublicRoute> } />
-        <Route path="/register" element={ <PublicRoute> <RegisterPage /> </PublicRoute> } />
-
-
-        
-        {/* Private Routes */}
-        <Route element={<PrivateRoute allowedRoles={["blogger", "admin"]} />}>
+          {/* Private Routes */}
+          <Route element={<PrivateRoute allowedRoles={["blogger", "admin"]} />}>
             <Route path="/customer" element={<CustomerPage />} />
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/makeblog" element={<MakeABlogPage />} />
@@ -81,20 +79,18 @@ function LocationWrapper({setToken, token, role}) {
             <Route path="/blog/:id" element={<BlogPage />} /> 
             <Route path="/deleteaccount" element={<DeleteAccountPage />} />
             <Route path="/yourblog" element={<YourBlogPage />} />
-        </Route>
+          </Route>
 
-
-
-        {/* Admin Route */}
-        <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+          {/* Admin Route */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
             <Route path="/profileview" element={<BloggerProfileViewPage />} />
             <Route path="/blogview" element={<BlogViewPage />} />
-        </Route>
+          </Route>
+        </Routes>
+      </Suspense>
 
-      </Routes>
-
-       {/* Footer will not be displayed on login and register pages */}
-       {location.pathname !== '/login' && location.pathname !== '/register' && <FooterSection />}
+      {/* Footer will not be displayed on login and register pages */}
+      {location.pathname !== '/login' && location.pathname !== '/register' && <FooterSection />}
     </>
   );
 }
